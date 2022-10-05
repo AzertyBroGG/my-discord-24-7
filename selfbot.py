9f9f498324d622486127a4b881eff7e3b5f9dc16
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-10-05 20:54:03 +0300

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,7 +1,14 @@
 import discord, asyncio, os
 from discord.ext import commands
-bot = commands.Bot(command_prefix = '.', self_bot = True)
+bot = commands.Bot(command_prefix = '.', help_cmmand = None, self_bot = True)
 @bot.event
 async def on_ready():
     print(bot.user.name)
+    print(bot.user.id)
+@bot.command()
+async def help(ctx):
+    await ctx.send('Я Азерти')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Задержка бота — {bot.latency}')
 bot.run(os.getenv('TOKEN'))

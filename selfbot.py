0938d3eb05182af26bd01b23dd86461aa0d670f6
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-10-05 17:50:40 +0300

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,5 +1,10 @@
 import discum as discord
 import os, asyncio
-
+print('Запуск...')
+@bot.gateway.command
+def main(resp):
+    if resp.event.ready_supplemental:
+        user = bot.gateway.session.user
+        print(f"Успешно вошел как {user['username']}")
 bot = discord.Client(token = os.getenv('TOKEN'), log = False)
 bot.gateway.run(auto_reconnect = True)

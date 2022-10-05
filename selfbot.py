6a2fe89c4e30d7a12713bd3e131c0be332537467
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-10-05 17:05:09 +0300

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,5 +1,5 @@
 import discum as discord
 import os, asyncio
 
-bot = discord.Client(token = os.getenv('TOKEN'))
+bot = discord.Client(token = os.getenv('TOKEN'), log = False)
 bot.gateway.run(auto_reconnect = True)

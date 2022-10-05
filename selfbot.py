a19b0a941009de2924be639ef1801f718750f638
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-10-05 20:41:29 +0300

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -7,4 +7,13 @@ def main(resp):
     if resp.event.ready_supplemental:
         user = bot.gateway.session.user
         print(f"Успешно вошел как {user['username']}")
+    if resp.event.message:
+        message = resp.parsed.auto()
+        content = message['content']
+        channel = message['channel_id']
+        if content.startswith('.'):
+            command = content.replace('.', '')
+            if command == 'ping':
+                bot.sendMessage(channel, 'Pong!')
+            
 bot.gateway.run(auto_reconnect = True)

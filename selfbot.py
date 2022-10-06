59895ca3d584d579052d1af828691989ca5cb13e
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-10-06 20:26:35 +0300

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,6 +1,6 @@
 import discord, asyncio, os
 from discord.ext import commands
-from discord_self_embed import Embed
+
 users = []
 bot = commands.Bot(command_prefix = '.', help_command = None, self_bot = True)
 @bot.event
@@ -24,10 +24,8 @@ async def on_message(message):
     
 @bot.command()
 async def help(ctx):
-    emb = Embed(title = 'Я Азерти SelfBot')
-    url = emb.generate_url()
-    await ctx.send(url)
+    await ctx.send('**Меню помощи МЕНЯ \nЗачем оно вам?**')
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f'Задержка бота — {round(bot.latency*1000)/1000} секунд')
+    await ctx.send(f'**Задержка бота — ** `{round(bot.latency*1000)/1000}` **секунд**')
 bot.run(os.getenv('TOKEN'))
